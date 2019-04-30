b22754f710e9a67c3752df0260365c56ee33cb73
Andrew Sims <andrew.cameron.sims@gmail.com>
2019-04-30 12:38:24 +1000

    Change test email to correct one

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,5 +1,5 @@
 class PurchaseMailer < ApplicationMailer
   def test_email
-    mail(to: 'testuserapp@gmail.com', subject: 'testing123')
+    mail(to: 'testuserapp6@gmail.com', subject: 'testing123')
   end
 end
