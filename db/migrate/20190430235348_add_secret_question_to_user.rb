d7f4b29e0c842f8d295a8cc50adb8c1547ac76ab
Andrew Sims <andrew.cameron.sims@gmail.com>
2019-05-01 09:55:31 +1000

    Generate database migrations for customisation of User

---
+++ db/migrate/20190430235348_add_secret_question_to_user.rb
@@ -0,0 +1,5 @@
+class AddSecretQuestionToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :secret_question, :string
+  end
+end
---
+++ db/migrate/20190430235426_add_encrypted_secret_to_user.rb
@@ -0,0 +1,5 @@
+class AddEncryptedSecretToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :encrypted_secret, :string
+  end
+end
