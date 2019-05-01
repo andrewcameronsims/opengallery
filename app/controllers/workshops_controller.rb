5e05308fa183cc96aa519f1c877f91d17e9e90f5
Andrew Sims <andrew.cameron.sims@gmail.com>
2019-05-01 16:00:43 +1000

    WIP adding methods to WorkshopsController

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -1,19 +1,32 @@
 class WorkshopsController < ApplicationController
-  def new
-  end
-
-  def create
+  def index
+    @workshops = Workshop.all
   end
 
   def show
+    @workshop = Workshop.find(params[:id])
+  end
+
+  def new
+    @workshop = Workshop.new
   end
 
   def edit
+    @workshop = Workshop.find(params[:id])
+  end
+
+  def create
+    @workshop = Workshop.new(workshop_params)
   end
 
   def update
   end
 
-  def delete
+  def destroy
   end
+  
+  private
+    def workshop_params
+      params.require(:workshop).permit(:bio, :bank_account, :auto_respond_msg, :user_id)
+    end
 end
