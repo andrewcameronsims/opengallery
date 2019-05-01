2d1b752a7129b9096b07359fe3f5b553895c4a34
Andrew Sims <andrew.cameron.sims@gmail.com>
2019-05-01 12:32:55 +1000

    Add custom route for artist signup

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
   get 'pieces/new'
   get 'pieces/edit'
   devise_for :users
+  devise_scope :user do
+    get 'artists/sign_up', to: 'devise/artists#new_artist', as: 'new_artist_registration'
+  end
   resources :charges
 end
