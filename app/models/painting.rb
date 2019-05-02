87d874a68e256774896534b61ab39ef3cc27b224
Andrew Sims <andrew.cameron.sims@gmail.com>
2019-05-02 11:57:13 +1000

    Add class which hits the Rijksmuseum API for images to display in our early testing

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,5 +1,11 @@
 class Painting
-  def self.get_url
-    
+  def self.get_urls
+    img_urls = []
+    rijksmuseum = HTTP.get(https://www.rijksmuseum.nl/api/en/collection/?key=8dqtwY6x&format=json)
+    rijksmuseum = JSON.parse(rijksmuseum)
+    rijksmuseum[:artObjects].each do |ao|
+      img_urls << ao[:webImage][:url]
+    end
+    img_urls
   end
 end
