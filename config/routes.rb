3d79a518243e8896e2a74c1011f222acc66f90c3
Andrew Sims <andrew.cameron.sims@gmail.com>
2019-05-03 14:42:37 +1000

    About and Contact have own routeS

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#home', as: 'home'
+  get '/about', to: 'home#about', as: 'about'
+  get '/contact', to: 'home#contact', as:'contact'
   
   devise_for :users
 
