561b95535f343725e67e66b264149bed81bf01c7
Andrew Sims <andrew.cameron.sims@gmail.com>
2019-05-03 14:52:58 +1000

    Minor change to homepage words

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -3,5 +3,11 @@ class PurchaseMailer < ApplicationMailer
     @user_name = params[:user]
     mail(to: 'testuserapp6@gmail.com', subject: 'Your Purchase Invoice')
   end
+
+  def seller_email
+  end
+
+  def buyer_email
+  end
 end
 
