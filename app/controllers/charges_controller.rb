f6062de38fefa4cdc392def9f67c295d77ffb0c5
Andrew Sims <andrew.cameron.sims@gmail.com>
2019-05-06 08:57:19 +1000

    Switch to encrypted cookies.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,7 +5,7 @@ class ChargesController < ApplicationController
   def create
 
     # Flag piece as sold and link to buyer.
-    piece = Piece.find(cookies[:piece_id])
+    piece = Piece.find(cookies.encrypted[:piece_id])
     piece.sold = true
     piece.user_id = current_user.id
     piece.save
@@ -19,25 +19,25 @@ class ChargesController < ApplicationController
     customer = Stripe::Customer.create({
     email: params[:stripeEmail],
     source: params[:stripeToken],
-  })
+    })
   
     charge = Stripe::Charge.create({
     customer: customer.id,
     amount: @amount,
     description: 'Rails Stripe customer',
     currency: 'usd',
-  })
+    })
 
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
+    
+    
+    # Customer was charged. Send an invoice.
+    PurchaseMailer.with(user: current_user.full_name).purchase_email.deliver_now
+    
+    # Artist sold a piece. Send a notification.
+    PurchaseMailer.with(user: artist.full_name).seller_email.deliver_now
   
-  
-  # Customer was charged. Send an invoice.
-  PurchaseMailer.with(user: current_user.full_name).purchase_email.deliver_now
-  
-  # Artist sold a piece. Send a notification.
-  PurchaseMailer.with(user: artist.full_name).seller_email.deliver_now
-  
-end
+  end
 end
