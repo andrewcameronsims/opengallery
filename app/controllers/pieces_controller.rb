29a8ecdab795357c048a10894311ac9029d127a2
Andrew Sims <andrew.cameron.sims@gmail.com>
2019-05-06 14:09:48 +1000

    Add comments to sorting algorithm

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,14 +1,20 @@
 class PiecesController < ApplicationController
   def index
-    if current_user
+    if current_user # Sorting only occurs if logged in
       user_tags = current_user.tags
+
+      # Sorted into three tiers: total match, partial match and no match between tags
       pieces_total_match = Piece.all.select { |p| p.tags.all? { |tag| user_tags.any? tag } }
+      
       pieces_partial_match = Piece.all.select do |p|
         p.tags.any? { |tag| user_tags.any? tag } && !pieces_total_match.include?(p)
       end
+      
       pieces_no_match = Piece.all.select do |p|
         !pieces_total_match.include?(p) && !pieces_partial_match.include?(p)
       end
+
+      # Concatenate these tiers into the sorted array of pieces
       @pieces = pieces_total_match + pieces_partial_match + pieces_no_match
     else
       @pieces = Piece.all.shuffle
