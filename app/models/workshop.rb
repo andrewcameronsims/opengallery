c484522778023511ac21ba2983cfaba1274db657
Seep G <seep.torque@gmail.com>
2019-05-08 14:29:34 +1000

    validate workshop works

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Workshop < ApplicationRecord
+  validates :bio, presence: true
+  validates :bank_account, length: {is: 10}, numericality: { greater_than_or_equal_to: 10 }, presence: true
   belongs_to :user
 end
