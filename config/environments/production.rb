1e4cab26af98dab05df9446098d9b3fdfa216e63
Andrew Sims <andrew.cameron.sims@gmail.com>
2019-05-09 14:37:57 +1000

    config mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "opengallery_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'open-gallery.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://open-gallery.herokuapp.com' }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.mailgun.org',
